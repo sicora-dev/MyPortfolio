5ba707caf798298b4e3bd36f0c01da985b5a0b27
sicora-dev <sirocornejoraez@gmail.com>
2024-12-26 21:42:02 +0100

    Add GitHub Actions workflow for deployment and update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://sicora-dev.github.io',
+  base: '/MyPortfolio',
 });
